545c291c354a1cc536623fe75d820e0dd24df13c
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-10-21 22:11:48 -0300

    Adicionado validações com DataAnnotation.

---
+++ Lab.Communication/Request/User/AcademicFormation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Communication.Request.User
+{
+    public class AcademicFormation
+    {
+        [Required(ErrorMessage = "Informe a instituição")]
+        [StringLength(100, ErrorMessage = "A instituição deve ter no máximo 100 caracteres")]
+        public string Institution { get; set; }
+
+        [Required(ErrorMessage = "Informe o curso")]
+        [StringLength(100, ErrorMessage = "O curso deve ter no máximo 100 caracteres")]
+        public string Course { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Informe qual o status do curso. (em progresso, concluído, etc...)")]
+        public int Status { get; set; }
+
+
+        [Range(1, int.MaxValue, ErrorMessage = "Informe seu nível de formação.")]
+        public int Formation { get; set; }
+    }
+}
---
+++ Lab.Communication/Request/User/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Communication.Request.User
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Informe o nome")]
+        [StringLength(100, ErrorMessage = "O nome deve ter no máximo 100 caracteres")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Informe o email")]
+        [StringLength(100, ErrorMessage = "O email deve ter no máximo 100 caracteres")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Informe um nome de usuário")]
+        [StringLength(100, ErrorMessage = "O email deve ter no máximo 100 caracteres")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Informe uma senha")]
+        [StringLength(100, ErrorMessage = "O email deve ter no máximo 100 caracteres")]
+
+        public string Password { get; set; }
+
+        public AcademicFormation AcademicFormation { get; set; }
+    }
+}
