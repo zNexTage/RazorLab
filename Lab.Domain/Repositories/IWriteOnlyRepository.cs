a1aaf3d890f4d007902350cf6b6f98943c7b1fbd
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-11-03 15:57:26 -0300

    Interface para escrita na base de dados.

---
+++ Lab.Domain/Repositories/IWriteOnlyRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab.Domain.Repositories
+{
+    /// <summary>
+    /// Interface genérica para ações de escrita na base de dados.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IWriteOnlyRepository<T>
+    {
+        public Task<T> Create(T obj);
+        public Task<T> Update(T obj);
+    }
+}
