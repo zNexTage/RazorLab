435a9a2dc705bec4b5c0621e8112653f054564bf
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-11-03 15:14:35 -0300

    Mapeando entidade de usuários.

---
+++ Lab.Infrastructure/Mapping/UserMap.cs
@@ -0,0 +1,37 @@
+﻿using FluentNHibernate.Mapping;
+using Lab.Domain.Entities;
+
+namespace Lab.Infrastructure.Mapping
+{
+    public class UserMap : ClassMap<ApplicationUser>
+    {
+        public UserMap() {
+            Table("Users");
+
+            Id(m => m.Id)
+                .Unique();            
+            
+            Map(m => m.Name)
+                .Length(100)
+                .Not.Nullable();                       
+
+            Map(m => m.Password)
+                .Length(100)
+                .Not.Nullable();
+
+            Map(m => m.Email)
+                .Length(100)
+                .Not.Nullable();
+
+            Map(m => m.UserName)
+                .Length(100)
+                .Not.Nullable();
+
+            Map(m => m.CreatedAt)
+                .CustomSqlType("datetime");
+
+            Map(m => m.UpdatedAt)
+                .CustomSqlType("datetime");            
+        }
+    }
+}
