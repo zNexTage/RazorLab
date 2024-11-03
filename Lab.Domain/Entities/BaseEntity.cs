a1ab55a470bb0fa0cc0b2c0aa9f9399bbac35aaf
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-11-03 15:10:55 -0300

    Criado entidade base.

---
+++ Lab.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public virtual string Id { get; set; } = Guid.NewGuid().ToString();
+        public virtual DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public virtual DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
