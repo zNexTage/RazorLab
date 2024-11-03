93898fe1345ebffaa870eccdec508bbca2488f82
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-11-03 15:11:11 -0300

    Criado entitdade de usuários.

---
+++ Lab.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Domain.Entities
+{
+    public class ApplicationUser : BaseEntity
+    {       
+
+        public virtual string Name { get; set; }
+                
+        public virtual string Email { get; set; }
+                
+        public virtual string UserName { get; set; }
+
+        public virtual string Password { get; set; }        
+    }
+}
