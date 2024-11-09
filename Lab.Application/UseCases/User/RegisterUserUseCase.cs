d579c17fb8c03885d5e4cb6eb9f7df409bc52c61
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-11-09 17:07:51 -0300

    Verificando se o email já foi registrado.

--- Lab.Application/UseCases/User/RegisterUserUseCase.cs
+++ Lab.Application/UseCases/User/RegisterUserUseCase.cs
@@ -2,6 +2,7 @@
 using Lab.Communication.Request.User;
 using Lab.Domain.Entities;
 using Lab.Domain.Repositories;
+using Lab.Domain.Repositories.User;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -13,26 +14,36 @@ namespace Lab.Application.UseCases.User
 {
     public class RegisterUserUseCase : IRegisterUserUseCase
     {
-        private readonly IWriteOnlyRepository<ApplicationUser> _repository;
+        private readonly IWriteOnlyRepository<ApplicationUser> _wRepository;
+        private readonly IUserReadOnlyRepository<ApplicationUser> _rRepository;
+
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
 
         public RegisterUserUseCase(
-            [FromKeyedServices("WUser")]IWriteOnlyRepository<ApplicationUser> repository,
+            [FromKeyedServices("WUser")]IWriteOnlyRepository<ApplicationUser> wRepository,
+            IUserReadOnlyRepository<ApplicationUser> rRepository,
             IMapper mapper,
             IUnitOfWork unitOfWork
             )
         {
-            _repository = repository;
+            _wRepository = wRepository;
+            _rRepository = rRepository;
             _mapper = mapper;
             _unitOfWork = unitOfWork;
         }
 
         public async Task<ApplicationUser> Register(RequestUser requestUser)
         {
+            var alreadyExists = await _rRepository.CheckEmail(requestUser.Email);
+
+            if (alreadyExists) {
+                throw new Exception("Já existe usuário com esse email");
+            }
+
             var user = _mapper.Map<ApplicationUser>(requestUser);
 
-            var result = await _repository.Create(user);
+            var result = await _wRepository.Create(user);
 
             await _unitOfWork.FlushAsync();
 
