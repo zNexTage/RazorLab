2e40fc6ede32cc4bca44b426321948b85c209147
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-11-08 21:27:36 -0300

    Criptografando senha com SHA256

---
+++ Lab.Application/Utils/PasswordHasher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+
+namespace Lab.Application.Utils
+{
+    public class PasswordHasher
+    {
+        /// <summary>
+        /// Criptografa uma senha utilizando o algoritmo sha256.
+        /// </summary>
+        /// <param name="password"></param>        
+        /// <returns></returns>
+        public static string Encrypt(string password)
+        {            
+            var bytes = Encoding.ASCII.GetBytes(password);
+
+            var data = SHA256.HashData(bytes);
+
+            return Convert.ToBase64String(data);
+        }
+    }
+}
